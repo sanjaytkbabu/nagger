ce04d0665f7650ea7474fcd790a76e0afad6a251
sanjaytkbabu <sanjaytkbabu@gmail.com>
2022-09-05 21:04:54 -0700

    dark mode added

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,7 +8,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import Box from '@mui/joy/Box';
+import { styled } from '@mui/material/styles';
 
 // var x = 10;
 
@@ -86,9 +87,9 @@ function DiscreteSlider(props: part) {
       <span className='cls_checkbox'>
     <label><Checkbox defaultChecked/>{props.text}:</label>
     </span>
-    <span className='cls_slider'>
-      <Slider
-        color='primary'
+    <Box className='cls_slider'>
+      <IOSSlider
+        color="primary" 
         aria-label="Minutes"
         defaultValue={30}
         getAriaValueText={valuetext}
@@ -98,14 +99,12 @@ function DiscreteSlider(props: part) {
         min={0}
         max={60}
         size='small'
-        disabled={false}
-        sx={{
-          "Slider-thumb-width": "24px",
-        }}
+        disabled={false} 
+        
         
         
       />
-      </span>
+      </Box>
       </div>
  
   );
@@ -163,3 +162,55 @@ export default function App() {
     </Router>
   );
 }
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
+  height: 2,
+  padding: '15px 0',
+  '& .MuiSlider-thumb': {
+    height: 10,
+    width: 10,
+    backgroundColor: '#fff',
+    boxShadow: iOSBoxShadow,
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow:
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontSize: 12,
+    fontWeight: 'normal',
+    top: -6,
+    backgroundColor: 'unset',
+    color: theme.palette.text.primary,
+    '&:before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    backgroundColor: '#bfbfbf',
+  },
+  '& .MuiSlider-mark': {
+    backgroundColor: '#bfbfbf',
+    height: 8,
+    width: 1,
+    '&.MuiSlider-markActive': {
+      opacity: 1,
+      backgroundColor: 'currentColor',
+    },
+  },
+}));
